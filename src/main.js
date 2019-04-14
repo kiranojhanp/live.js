2b457e283c16e1e93c8ee5753dec2a0ebac34b3e
MKW <MKW@MKWs-MBP.fios-router.home>
2019-04-14 01:55:03 -0700

    screw analytics

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import KnobControl from 'vue-knob-control';
 import VTooltip from 'v-tooltip'
-import VueAnalytics from 'vue-analytics'
 
 Vue.use(VTooltip)
 Vue.config.productionTip = false;
@@ -12,11 +11,6 @@ Vue.use(KnobControl)
 
 Vue.component('knob-control', KnobControl)
 
-
-Vue.use(VueAnalytics, {
-  id: `${process.env.TRACKING_ID}`,
-  checkDuplicatedScript: true
-})
 
 new Vue({
   router,
